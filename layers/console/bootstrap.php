e4152db4204f7ab3bb03ba52a4911a83813d52c3
Matthieu Napoli <matthieu@mnapoli.fr>
2022-10-27 15:17:38 +0200

    #6 Self-contained console layer

--- layers/console/bootstrap.php
+++ layers/console/bootstrap.php
@@ -1,17 +1,5 @@
 <?php declare(strict_types=1);
 
-use Bref\ConsoleRuntime\Main;
+require '/opt/bref/console-runtime/vendor/autoload.php';
 
-if (getenv('BREF_AUTOLOAD_PATH')) {
-    require getenv('BREF_AUTOLOAD_PATH');
-} else {
-    $appRoot = getenv('LAMBDA_TASK_ROOT');
-
-    require $appRoot . '/vendor/autoload.php';
-}
-
-if (! class_exists(Main::class)) {
-    throw new RuntimeException('Bref is not installed in your application (could not find the class "Bref\FunctionRuntime\Main" in Composer dependencies). Did you run "composer require bref/bref"?');
-}
-
-Main::run();
+\Bref\ConsoleRuntime\Main::run();
