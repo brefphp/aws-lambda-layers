011bb47c29c8513ed73b78a1b934258eae126c66
Matthieu Napoli <matthieu@mnapoli.fr>
2022-12-01 12:56:08 +0100

    Lighter layers by not including existing system libraries

--- utils/lib-check/verify.php
+++ utils/lib-check/verify.php
@@ -15,7 +15,12 @@ ls /lib64 -la
 # We will then read the Dockerfile, remove all comments and compare any file that we
 # may be copying into the layer that doesn't need to be there.
 
-$docker = file_get_contents(__DIR__ . '/../../php-80/cpu-x86.Dockerfile');
+if (! ($argv[1] ?? false)) {
+    echo 'Run via "make check"' . PHP_EOL;
+    exit(1);
+}
+
+$docker = file_get_contents(__DIR__ . '/../../' . $argv[1]);
 
 $dockerContent = explode(PHP_EOL, $docker);
 
@@ -36,3 +41,5 @@ foreach ($libraries as $library) {
         echo "[$library] is present in Docker but is also present on /lib64 by default" . PHP_EOL;
     }
 }
+
+echo 'OK' . PHP_EOL;
