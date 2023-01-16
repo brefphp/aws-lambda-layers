47c21ec01bc274b4f9b4f99e53414329838ee398
Matthieu Napoli <matthieu@mnapoli.fr>
2023-01-16 18:22:36 +0100

    Fix the zip script

--- utils/docker-zip-dir.sh
+++ utils/docker-zip-dir.sh
@@ -3,13 +3,19 @@
 # Fail on error
 set -e
 
+rm -f "output/$2.zip"
 rm -rf "output/$2"
 mkdir "output/$2"
 
+# Remove any previously failed container if it exists
+docker rm -f bref-export-zip 2>/dev/null || true
+
 docker create --name bref-export-zip "$1"
 
-docker cp bref-export-zip:/opt "output/$2"
+docker cp bref-export-zip:/opt/. "output/$2"
 
-zip --quiet --recurse-paths "output/$2.zip" "output/$2"
+cd "output/$2"
+
+zip --quiet --recurse-paths "../$2.zip" .
 
 docker rm -f bref-export-zip
