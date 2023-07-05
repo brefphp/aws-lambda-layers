2eb238d244fb62443ba05d3bef17b71d537c55eb
Jason McCallister <jason@craftcms.com>
2023-07-05 10:14:47 -0400

    revert while style

--- layers/console/bootstrap.sh
+++ layers/console/bootstrap.sh
@@ -8,19 +8,20 @@ if [ -z "${RUNTIME_CLASS}" ]; then
   export RUNTIME_CLASS="Bref\ConsoleRuntime\Main"
 fi
 
-while true; do
-  if [ -z "${EXPERIMENTAL_AWS_LAMBDA_EXEC_WRAPPER}" ]; then
-    # Default behavior
+while true
+do
+    if [ -z "${EXPERIMENTAL_AWS_LAMBDA_EXEC_WRAPPER}" ]; then
+        # Default behavior
 
-    # We redirect stderr to stdout so that everything
-    # written on the output ends up in Cloudwatch automatically
-    php "/opt/bref/bootstrap.php" 2>&1
-  else
-    # A wrapper script is configured
-    # See https://docs.aws.amazon.com/lambda/latest/dg/runtimes-modify.html#runtime-wrapper
-    # NOTE: EXPERIMENTAL FEATURE, DO NOT USE!!!
-    # Note: If you do use it, open an issue or GitHub discussion or Slack thread
-    # and let us know why it's useful to you, we might turn it into an official feature
-    "${EXPERIMENTAL_AWS_LAMBDA_EXEC_WRAPPER}" php "/opt/bref/bootstrap.php" 2>&1
-  fi
+        # We redirect stderr to stdout so that everything
+        # written on the output ends up in Cloudwatch automatically
+        php "/opt/bref/bootstrap.php" 2>&1
+    else
+        # A wrapper script is configured
+        # See https://docs.aws.amazon.com/lambda/latest/dg/runtimes-modify.html#runtime-wrapper
+        # NOTE: EXPERIMENTAL FEATURE, DO NOT USE!!!
+        # Note: If you do use it, open an issue or GitHub discussion or Slack thread
+        # and let us know why it's useful to you, we might turn it into an official feature
+        "${EXPERIMENTAL_AWS_LAMBDA_EXEC_WRAPPER}" php "/opt/bref/bootstrap.php" 2>&1
+    fi
 done
