414a787fe1d0243fa1a4ef7acc4bbdb932f68707
Matthieu Napoli <matthieu@mnapoli.fr>
2025-05-24 22:27:36 +0200

    Refactor to a single layer (#256)
    * Refactor to a single layer
    
    * Fix
    
    * Fixes
    
    * Finish merging php.ini files
    
    * Update the PHP 8.2 and 8.3 build scripts too

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php declare(strict_types=1);
 
 $appRoot = getenv('LAMBDA_TASK_ROOT');
@@ -12,7 +13,19 @@ if (! file_exists($autoloadPath)) {
 
 require $autoloadPath;
 
-$runtimeClass = getenv('RUNTIME_CLASS');
+// `RUNTIME_CLASS` is for backwards compatibility with Bref v2's environment variable
+$runtime = $_SERVER['BREF_RUNTIME'] ?? $_SERVER['RUNTIME_CLASS'] ?? null;
+
+if (empty($runtime)) {
+    throw new RuntimeException('The environment variable `BREF_RUNTIME` is not set, are you trying to use Bref v2 with Bref v3 layers? Make sure to follow the Bref documentation to use the right layers for your current Bref version.');
+}
+
+$runtimeClass = match ($runtime) {
+    'function' => 'Bref\FunctionRuntime\Main',
+    'fpm' => 'Bref\FpmRuntime\Main',
+    'console' => 'Bref\ConsoleRuntime\Main',
+    default => $runtime,
+};
 
 if (! class_exists($runtimeClass)) {
     throw new RuntimeException("Bref is not installed in your application (could not find the class \"$runtimeClass\" in Composer dependencies). Did you run \"composer require bref/bref\"?");
